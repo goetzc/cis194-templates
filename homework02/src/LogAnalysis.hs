0a276db1a0ea9470171e28274c588e9030be6411
Götz Christ <goetzchrist@gmail.com>
2017-08-30 21:17:50 -0500

    Resolve Homework 02

--- homework02/src/LogAnalysis.hs
+++ homework02/src/LogAnalysis.hs
@@ -25,10 +25,15 @@ import Log
 -- Unknown "This is not in the right format"
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage message = case words message of
+    ("I":time:xs)   -> LogMessage Info (read time) (unwords xs)
+    ("W":time:xs)   -> LogMessage Warning (read time) (unwords xs)
+    ("E":n:time:xs) -> LogMessage (Error $ read n) (read time) (unwords xs)
+    other           -> Unknown $ unwords other
 
 parse :: String -> [LogMessage]
-parse = undefined
+-- parse messages = ((map parseMessage) . lines) messages
+parse messages = map parseMessage $ lines messages
 
 ----------------------------------------------------------------------
 -- Exercise 2
@@ -40,7 +45,11 @@ parse = undefined
 --
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert m Leaf = Node Leaf m Leaf
+insert m@(LogMessage _ t _) (Node left m2@(LogMessage _ t2 _) right)
+    | t < t2    = Node (insert m left) m2 right
+    | otherwise = Node left            m2 (insert m right)
+insert _ tree = tree
 
 ----------------------------------------------------------------------
 -- Exercise 3
@@ -52,7 +61,9 @@ insert = undefined
 --
 
 build :: [LogMessage] -> MessageTree
-build = undefined
+-- build [] = Leaf
+-- build (m:xs) = insert m (build xs)
+build = foldr insert Leaf
 
 ----------------------------------------------------------------------
 -- Exercise 4
@@ -64,7 +75,8 @@ build = undefined
 --
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left m right) = inOrder left ++ [m] ++ inOrder right
 
 ----------------------------------------------------------------------
 -- Exercise 5
@@ -76,7 +88,20 @@ inOrder = undefined
 --
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong = extractMsg . sortByTimestamp . filter relevant
+
+relevant :: LogMessage -> Bool
+relevant (LogMessage (Error n) _ _)
+    | n >= 50   = True
+    | otherwise = False
+relevant _ = False
+
+sortByTimestamp :: [LogMessage] -> [LogMessage]
+sortByTimestamp = inOrder . build
+
+extractMsg :: [LogMessage] -> [String]
+extractMsg (LogMessage _ _ s : xs) = s : extractMsg xs
+extractMsg _ = []
 
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
